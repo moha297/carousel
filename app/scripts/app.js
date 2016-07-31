915b2b815df5e5142c4b5d7da1a3f98483c5b806
Anuraag <anuraag@magnifi.fm>
2016-07-30 18:07:46 -0700

    Initial App Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name carouselApp
+ * @description
+ * # carouselApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('carouselApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
