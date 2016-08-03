18573547282aec5338beddadb2964408861e0294
Anuraag <anuraag@magnifi.fm>
2016-08-02 20:19:31 -0700

    Almost done
    Handling of thumbnails
    Resize events
    Few css tweaks

--- app/scripts/directives/carousel.js
+++ app/scripts/directives/carousel.js
@@ -7,7 +7,7 @@
  * # carousel
  */
 angular.module('carouselApp')
-  .directive('carousel', function() {
+  .directive('carousel', ['$window', function($window) {
     return {
       templateUrl: 'views/directives/carousel.html',
       restrict: 'E',
@@ -18,43 +18,89 @@ angular.module('carouselApp')
       },
       link: function postLink(scope, element /*, attrs*/ ) {
         var slideCont = element.find('.slides-cont');
-        // Good old jquery
-        var containerWidth = slideCont.width(),
-          containerHeight = slideCont.height();
-
-        // Set the size of slides based on container
-        scope.slideW = containerWidth;
-        scope.slideH = containerHeight;
 
         //Slider original position
         scope.slideVal = 0;
 
         scope.currentIndex = 0;
 
+        function calculate() {
+          // Good old jquery
+          var containerWidth = slideCont.width(),
+            containerHeight = slideCont.height();
+
+          // Set the size of slides based on container
+          scope.slideW = containerWidth;
+          scope.slideH = containerHeight;
+
+          scope.reelWidth = scope.slideW * scope.slideCount; // reel size based on number of photos
+          scope.thumbnailReelWidth = ((scope.slideCount - 1) * 112) + 163; // reel size based on thumbnails
+        }
+
         // We watch the photos data to get things in right order
         scope.$watch('photos', function(newValue, oldValue) {
           var cnt = newValue.length; //number of photos in album
           scope.slideCount = cnt;
-          scope.reelWidth = scope.slideW * scope.slideCount; // reel size based on number of photos
+
+          //calculate the view size attributes
+          calculate();
+
           // This will take the photos to the view layer
           scope.slideList = newValue;
         });
 
 
         // Handler for left arrow click event. Moves the slider left.
-        scope.leftArrow = function(){
-          if(scope.currentIndex > 0) {
+        scope.leftArrow = function() {
+          if (scope.currentIndex > 0) {
             scope.currentIndex--;
             scope.slideVal = scope.currentIndex * scope.slideW;
           }
         };
         // Handler for right arrow click event. Moves the slider right.
-        scope.rightArrow = function(){
-          if(scope.currentIndex < scope.slideCount-1) {
+        scope.rightArrow = function() {
+          if (scope.currentIndex < scope.slideCount - 1) {
             scope.currentIndex++;
             scope.slideVal = scope.currentIndex * scope.slideW;
           }
         };
+
+        // Thumbnails manipulate the slides using this
+        scope.moveTo = function(index) {
+          if (index < scope.slideCount && index >= 0) {
+            scope.currentIndex = index;
+            scope.slideVal = scope.currentIndex * scope.slideW;
+          }
+        };
+
+
+        //Listen to window resize and re-calculate the gallery sizes
+        var timer;
+
+        //leverages settimeout to optimize the size calculations
+        // Resize event is fired continously as window is changed
+        // We defer by 300ms to decrease invocations for calculations
+        function resizeListener() {
+          if (timer) {
+            window.clearTimeout(timer);
+          }
+          timer = window.setTimeout(function() {
+            console.log("resize")
+            scope.$apply(function() {
+              calculate();
+            });
+          }, 300);
+
+        }
+        angular.element($window).on('resize', resizeListener);
+        scope.$on('destroy', function() {
+          if (timer) {
+            window.clearTimeout(timer);
+          }
+          angular.element($window).off('resize', resizeListener);
+        });
+
+
       }
     };
-  });
+  }]);
